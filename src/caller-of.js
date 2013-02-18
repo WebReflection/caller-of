46b1c95ca0ac7aed4cada1de45c89994fd1c99e1
barak <barak@jsmaker.com>
2013-02-19 01:32:15 +0200

    Update src/caller-of.js

--- src/caller-of.js
+++ src/caller-of.js
@@ -1,2 +1,2 @@
 // (C) WebReflection, Mit Style License
-function callerOf(c) {'use strict'; return c.bind.call(c.call, c); }
+function callerOf(c) {'use strict'; return c.bind.apply(c.call, arguments); }
